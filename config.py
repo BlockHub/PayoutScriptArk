dd57a59f40fe011d180583004a2f6c0ca0a7c13f
AbeIgnocious <k.l.kubat@gmail.com>
2017-11-24 23:00:30 +0100

    removed some options and simplified the script

--- config.py
+++ config.py
@@ -19,75 +19,35 @@ DELEGATE = {
 
 # How are fees calculated.
 SENDER_SETTINGS = {
-    'FEES'     : info.TX_FEE,
-    'FLAT_TAX' : None,
-    'BLACKLIST': None,
 
     # The default share, when TIMESTAMP_BRACKETS (see below) do not apply.
-    'DEFAULT_SHARE': 0.96,
-
-    # TIMESTAMP_BRACKETS do not redistribute the amounts to lower brackets,
-    # proceeds go to delegate
-    # If TIMESTAMP_BRACKETS and BALANCE_BRACKETS are both not None, they are
-    # both applied: I.E. above
-    # 200k Ark = 80% share, and if voted after 8th of september = 90% share,
-    # the total share for this voter
-    # is 80% * 90% = 0.64%.
-    'TIMESTAMP_BRACKETS': None,
-
-    # BALANCE_BRACKETS do not redistribute the amounts to lower brackets,
-    # proceeds go to delegate
-    'BALANCE_BRACKETS': None,
+    'DEFAULT_SHARE': 1,
 
     # any balance above max balance or below min balance is not taken in the
     # calculation (the ark is divided amongst voters and delegate)
     # so if MAX_BALANCE == 200k and a voter has
     # 300k, it is counted as 200K.
     # If a voter has 1 Ark, and MIN_BALANCE == 2 Ark, it is counted as 0 Ark
-    'MAX_BALANCE'                  : None,
-    'MIN_BALANCE'                  : None,
-    'MIN_PAYOUT_BALANCE_DAILY'     : 2,
-    'MIN_PAYOUT_BALANCE_WEEKLY'    : 0.1,
-    'MIN_PAYOUT_BALANCE_MONTHLY'   : 0.001,
+    'MIN_PAYOUT_BALANCE'           : 0,
     'COVER_TX_FEES'                : False,
-    'MAX_BALANCE_COVER_TX_FEES'    : False,
-    'MIN_BALANCE_COVER_VOTING_FEES': False,
 
     # At the end of the personal message a tag could be included for
     # administrative purposes
     # Example: 'Thanks for voting! ::VOTINGFEE::
     'PERSONAL_MESSAGE': None,
+    #6 is sunday, 1 is monday etc.
     'DAY_WEEKLY_PAYOUT': 5,
-    'DAY_MONTHLY_PAYOUT': 15,
-    'WAIT_TIME_DAILY': 20*info.HOUR_SEC
+    # min amount of seconds between a payout
+    'WAIT_TIME': 0,
 
 }
 
-# Custom payout schemes on a per-voter basis. E.g., you can give a higher
-# share than the default to early adopters.
-SHARE_EXCEPTIONS = None
-
-# Blacklisted voters: whom do we never wish to pay out.
+# Blacklisted voters: whom do we never wish to pay out. Their Ark is divided over all other voters + delegate
+# format is a list
 BLACKLIST = None
 
-# Secret key for broadcasting to the Ark network.
-SECRET = 'string'
-
-# 1 = daily
-# 2 = weekly, saturday
-# 3 = monthly, on the 28th
-
-FREQUENCY_DICT = {
-        'address': 1,               
-    }
-
+# preset amounts to be sent. format is a dict with address: amount. amount is in arksatoshis (10^8 as = 1 ark)
 HARD_EXCEPTIONS = None
-
-# The payout files will appear in this directory for further processing.
-# The payout script will create the directory if it doesn't exist yet.
-PAYOUTDIR     = '/home/ark/payouts'
-# Failed payouts will be moved here for closer inspection:
-PAYOUTFAILDIR = '/home/ark/failedpayouts'
 
 # Where to log stuff. This will be rotated so that the disk doesn't fill
 # up like crazy.
@@ -99,5 +59,4 @@ LOGGING = {
 
 # This enables the testmode in the payout sender. No payouts are sent,
 # only log statements are generated.
-PAYOUTCALCULATOR_TEST = True
-PAYOUTSENDER_TEST = True
+PAYOUTCALCULATOR_TEST = False
--- plugandplay.py
+++ plugandplay.py
@@ -2,8 +2,8 @@ import arkdbtools.dbtools as ark
 import arkdbtools.config as info
 import logging.handlers
 import config
-import pickle
-
+import datetime
+import arky
 '''
 We'll use the same RotatingFileHandler as arkdbtools. However in production I recommend
 using https://sentry.io/
@@ -21,13 +21,13 @@ def main():
     # check if node is at reasonable height. 51 means you are at maximum
     # 2 forged blocks behind.
     logger.info('checking node')
-    if config.PAYOUTCALCULATOR_TEST:
-        logger.info('--TESTMODE ON-- Node status: {}. Continuing main'.format(ark.Node.check_node(51)))
-    else:
-        if not ark.Node.check_node(51):
-            # logger.fatal('NodeDbError, node was more than 51 blocks behind')
-            raise ark.NodeDbError('NodeDbError, node was more than 51 blocks behind')
-        logger.info('Node was within 51 blocks of the network')
+    # if config.PAYOUTCALCULATOR_TEST:
+    #     logger.info('--TESTMODE ON-- Node status: {}. Continuing main'.format(ark.Node.check_node(51)))
+    # else:
+    #     if not ark.Node.check_node(51):
+    #         logger.fatal('NodeDbError, node was more than 51 blocks behind')
+    #         raise ark.NodeDbError('NodeDbError, node was more than 51 blocks behind')
+    #     logger.info('Node was within 51 blocks of the network')
 
 
 
@@ -38,62 +38,73 @@ def main():
     )
     logger.debug('setting delegate: {0}, {1}'.format(config.DELEGATE['ADDRESS'], config.DELEGATE['PUBKEY']))
 
-    ark.set_calculation()
+    ark.set_calculation(
+        blacklist=config.BLACKLIST
+    )
 
-    payouts, timestamp = ark.Delegate.share()
+    print('starting calculation')
+    payouts, timestamp = ark.Delegate.trueshare()
 
     if config.PAYOUTCALCULATOR_TEST:
         for i in payouts:
             logger.info('{0} ----  {1}(share) ---- {2}(balance)'.format(i, payouts[i]['share']/info.ARK, payouts[i]['balance']/info.ARK))
 
-    ark.set_sender(
-        default_share=config.SENDER_SETTINGS['DEFAULT_SHARE'],
-        cover_fees=config.SENDER_SETTINGS['COVER_TX_FEES'],
-        share_percentage_exceptions=config.SHARE_EXCEPTIONS,
-        timestamp_brackets=config.SENDER_SETTINGS['TIMESTAMP_BRACKETS'],
-        min_payout_daily=config.SENDER_SETTINGS['MIN_PAYOUT_BALANCE_DAILY'],
-        min_payout_weekly=config.SENDER_SETTINGS['MIN_PAYOUT_BALANCE_WEEKLY'],
-        min_payout_monthly=config.SENDER_SETTINGS['MIN_PAYOUT_BALANCE_MONTHLY'],
-        day_weekly_payout=config.SENDER_SETTINGS['DAY_WEEKLY_PAYOUT'],
-        day_monthly_payout=config.SENDER_SETTINGS['DAY_MONTHLY_PAYOUT'],
-        payoutsender_test=config.PAYOUTCALCULATOR_TEST,
-        sender_exception=config.HARD_EXCEPTIONS,
-        wait_time_day=config.SENDER_SETTINGS['WAIT_TIME_DAILY'],
-        wait_time_week=config.SENDER_SETTINGS['WAIT_TIME_DAILY'],
-        wait_time_month=config.SENDER_SETTINGS['WAIT_TIME_DAILY'],
-    )
-    failed_payouts = 0
-    successful_payouts = 0
-    stored_payouts = 0
     logger.info('starting transmitting payouts')
     delegate_share = 0
+    print('starting transmission')
+    if config.SENDER_SETTINGS['COVER_TX_FEES']:
+        fees = 0
+    else:
+        fees = info.TX_FEE
+    ark.set_sender(payoutsender_test=config.PAYOUTCALCULATOR_TEST)
+    current_day = datetime.datetime.today().weekday()
+    # lets deal with the exceptions first:
+    try:
+        for x in config.HARD_EXCEPTIONS:
+            if config.HARD_EXCEPTIONS[x] <= payouts[x]['share'] \
+            and config.SENDER_SETTINGS['DAY_WEEKLY_PAYOUT'] == current_day \
+            and payouts[x]['last_payout'] < timestamp - config.SENDER_SETTINGS['WAIT_TIME']:
+                try:
+                    ark.Core.send(
+                        address=x,
+                        amount=config.HARD_EXCEPTIONS[x],
+                        smartbridge=config.SENDER_SETTINGS['PERSONAL_MESSAGE'],
+                        secret=config.DELEGATE['PASSPHRASE'])
+                    delegate_share += (payouts[x]['share'] - config.HARD_EXCEPTIONS[x] - info.TX_FEE)
+                except Exception:
+                    logger.exception('failed a HARD EXCEPTION transaction')
+    except Exception:
+        pass
+
     for payout in payouts:
-        data = payout, payouts[payout]
-        try:
-            result, delegate_reward, amount = ark.Core.payoutsender(data, calculation_timestamp=timestamp)
-            delegate_share += delegate_reward
+        amount = (payouts[payout]['share'] * config.SENDER_SETTINGS['DEFAULT_SHARE'])
+        if amount > config.SENDER_SETTINGS['MIN_PAYOUT_BALANCE'] \
+        and config.SENDER_SETTINGS['DAY_WEEKLY_PAYOUT'] == current_day \
+        and payouts[payout]['last_payout'] < timestamp - config.SENDER_SETTINGS['WAIT_TIME']:
+            try:
+                res = ark.Core.send(
+                    address=payout,
+                    amount=amount,
+                    smartbridge=config.SENDER_SETTINGS['PERSONAL_MESSAGE'],
+                    secret=config.DELEGATE['PASSPHRASE'])
+                delegate_share += payouts[payout]['share'] - amount
+                logger.debug(res)
+            except Exception:
+                logger.exception('failed transaction:')
+    ark.Core.send(
+        address=config.DELEGATE['REWARDWALLET'],
+        amount=delegate_share,
+        secret=config.DELEGATE['PASSPHRASE'])
 
-        except ark.TxParameterError:
-            stored_payouts += 1
-            logger.info('Txparametererror, stored payout')
-        if result:
-            successful_payouts += 1
-            logger.info('sent {0} to {1}, delegate_reward: {2}'.format(amount, payout, delegate_reward))
-        else:
-            failed_payouts += 1
-            logger.fatal('payout failed for {0}, response: {1}'.format(payout, result))
 
-    ark.Core.send(config.DELEGATE['REWARDWALLET'], delegate_share)
-    logger.info('sent {0} to delegate: {1}'.format(delegate_share, config.DELEGATE['REWARDWALLET']))
 
-    logger.info('Successful payouts: {0}, failed payouts: {1}'.format(successful_payouts, failed_payouts))
-    logger.info(('failed payouts: {}'.format(failed_payouts)))
-    logger.info('stored payouts: {}'.format(stored_payouts))
-    logger.info('delegate_share: {}'.format(delegate_share))
+
+
 
 
 if __name__ == '__main__':
     # Initialize logging
+    print('initializing logging')
     logger = logging.getLogger(__name__)
     handler = logging.handlers.RotatingFileHandler(config.LOGGING['LOGDIR'],
                                                    encoding='utf-8',
@@ -102,6 +113,7 @@ if __name__ == '__main__':
     formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+
     if config.PAYOUTCALCULATOR_TEST:
         logger.setLevel(logging.DEBUG)
     else:
