f25f2aa2466c6a1ea943e53a0446cd8e77c41481
AbeIgnocious <k.l.kubat@gmail.com>
2017-12-07 00:32:33 +0100

    refactored code, changed logging to improve testruns

--- plugandplay.py
+++ plugandplay.py
@@ -3,24 +3,37 @@ import arkdbtools.config as info
 import logging.handlers
 import config
 import datetime
-import arky
+
 '''
 We'll use the same RotatingFileHandler as arkdbtools. However in production I recommend
 using https://sentry.io/
 '''
 
 
-def main():
-    # logger.info('setting connection')
+def connect():
     ark.set_connection(
         host=config.CONNECTION['HOST'],
         database=config.CONNECTION['DATABASE'],
         user=config.CONNECTION['USER'],
         password=config.CONNECTION['PASSWORD'])
 
+
+def set_params():
+    ark.set_delegate(
+        address=config.DELEGATE['ADDRESS'],
+        pubkey=config.DELEGATE['PUBKEY'],
+        secret=config.DELEGATE['PASSPHRASE'],
+    )
+    logger.info('setting delegate: {0}, {1}'.format(config.DELEGATE['ADDRESS'], config.DELEGATE['PUBKEY']))
+
+    ark.set_calculation(
+        blacklist=config.BLACKLIST
+    )
+
+
+def check_node_height():
     # check if node is at reasonable height. 51 means you are at maximum
     # 2 forged blocks behind.
-    logger.info('checking node')
     if config.PAYOUTCALCULATOR_TEST:
         logger.info('--TESTMODE ON-- Node status: {}. Continuing main'.format(ark.Node.check_node(51)))
     else:
@@ -29,100 +42,86 @@ def main():
             raise ark.NodeDbError('NodeDbError, node was more than 51 blocks behind')
         logger.info('Node was within 51 blocks of the network')
 
-    ark.set_delegate(
-        address= config.DELEGATE['ADDRESS'],
-        pubkey=  config.DELEGATE['PUBKEY'],
-        secret=  config.DELEGATE['PASSPHRASE'],
-    )
-    logger.debug('setting delegate: {0}, {1}'.format(config.DELEGATE['ADDRESS'], config.DELEGATE['PUBKEY']))
 
-    ark.set_calculation(
-        blacklist=config.BLACKLIST
-    )
-
-    print('starting calculation')
+def calculate():
     payouts, timestamp = ark.Delegate.trueshare(start_block=config.CALCULATION_SETTINGS['STARTBLOCK_CALCULATION'])
 
     if config.PAYOUTCALCULATOR_TEST:
+        logger.info('TRUE PAYMENTS')
         for i in payouts:
             logger.info('{0} ----  {1}(share) ---- {2}(balance)'.format(i, payouts[i]['share']/info.ARK, payouts[i]['balance']/info.ARK))
-        return
+    return payouts, timestamp
 
-    logger.info('starting transmitting payouts')
+
+def format_payments(payouts, timestamp):
     delegate_share = 0
+    res = {}
 
-    print('starting transmission')
-
+    # delegates substract fees from the amount you are sending. So if you transmit 1 Ark, the receiver will get 0.9 Ark
     if config.SENDER_SETTINGS['COVER_TX_FEES']:
         fees = info.TX_FEE
     else:
         fees = 0
-    ark.set_sender(payoutsender_test=config.PAYOUTCALCULATOR_TEST)
-    current_day = datetime.datetime.today().weekday()
-
-    # lets deal with the exceptions first:
-    try:
-        for x in config.HARD_EXCEPTIONS:
-            if config.HARD_EXCEPTIONS[x] <= payouts[x]['share'] \
-            and config.SENDER_SETTINGS['DAY_WEEKLY_PAYOUT'] == current_day \
-            and payouts[x]['last_payout'] < timestamp - config.SENDER_SETTINGS['WAIT_TIME']:
-                try:
-                    ark.Core.send(
-                        address=x,
-                        amount=config.HARD_EXCEPTIONS[x],
-                        smartbridge=config.SENDER_SETTINGS['PERSONAL_MESSAGE'],
-                        secret=config.DELEGATE['PASSPHRASE'])
-                    delegate_share += (payouts[x]['share'] - config.HARD_EXCEPTIONS[x] + fees)
-                except Exception:
-                    logger.exception('failed a HARD EXCEPTION transaction')
-
-    except Exception:
-        logger.exception('failed transmitting hard exception payments')
 
     for payout in payouts:
 
         amount = (payouts[payout]['share'] * config.SENDER_SETTINGS['DEFAULT_SHARE']) + fees
 
-        if amount > config.SENDER_SETTINGS['MIN_PAYOUT_BALANCE'] \
-        and config.SENDER_SETTINGS['DAY_WEEKLY_PAYOUT'] == current_day \
+        if amount >= config.SENDER_SETTINGS['MIN_PAYOUT_BALANCE'] \
         and payouts[payout]['last_payout'] < timestamp - config.SENDER_SETTINGS['WAIT_TIME']:
 
             if config.SENDER_SETTINGS['REQUIRE_CURRENT_VOTER']:
                 if payouts[payout]['status']:
-                    try:
-                        res = ark.Core.send(
-                            address=payout,
-                            amount=amount,
-                            smartbridge=config.SENDER_SETTINGS['PERSONAL_MESSAGE'],
-                            secret=config.DELEGATE['PASSPHRASE'])
-                        delegate_share += payouts[payout]['share'] - amount
-                        logger.debug(res)
-                    except Exception:
-                        logger.exception('failed transaction:')
+                    res.update({payout: amount})
+                    delegate_share += payouts[payout]['share'] - amount
+
                     continue
 
             else:
-                try:
-                    res = ark.Core.send(
-                        address=payout,
-                        amount=amount,
-                        smartbridge=config.SENDER_SETTINGS['PERSONAL_MESSAGE'],
-                        secret=config.DELEGATE['PASSPHRASE'])
-                    delegate_share += payouts[payout]['share'] - amount
-                    logger.debug(res)
-                except Exception:
-                    logger.exception('failed transaction:')
+                res.update({payout: amount})
+                delegate_share += payouts[payout]['share'] - amount
+                logger.debug(res)
+                logger.exception('failed transaction:')
 
+
+    try:
+        for x in config.HARD_EXCEPTIONS:
+            if config.HARD_EXCEPTIONS[x] <= payouts[x]['share'] \
+            and payouts[x]['last_payout'] < timestamp - config.SENDER_SETTINGS['WAIT_TIME']:
+
+                amount = config.HARD_EXCEPTIONS[x] + fees
+                delegate_share += (payouts[x]['share'] - amount)
+                res.update({
+                    config.HARD_EXCEPTIONS[x]: amount,
+                })
+    except Exception:
+        logger.exception('failed in setting payout for hard exceptions')
+
+    return res, delegate_share
+
+
+def transmit_payments(payouts):
+    current_day = datetime.datetime.today().weekday()
+    if config.SENDER_SETTINGS['DAY_WEEKLY_PAYOUT'] == current_day:
+        for i in payouts:
+            ark.Core.send(
+                address=i,
+                amount=payouts[i],
+                secret=config.DELEGATE['PASSPHRASE'],
+                smartbridge=config.SENDER_SETTINGS['PERSONAL_MESSAGE'],
+            )
+
+
+def send_delegate_share(amount):
     # sending payouts to the rewardwallet
     ark.Core.send(
         address=config.DELEGATE['REWARDWALLET'],
-        amount=delegate_share,
+        amount=amount,
         secret=config.DELEGATE['PASSPHRASE'])
 
 
 if __name__ == '__main__':
     # Initialize logging
-    print('initializing logging')
     logger = logging.getLogger(__name__)
     handler = logging.handlers.RotatingFileHandler(config.LOGGING['LOGDIR'],
                                                    encoding='utf-8',
@@ -140,7 +139,33 @@ if __name__ == '__main__':
     # Protect the entire run in a try block so we get postmortem info if
     # applicable.
     try:
-        main()
+        logger.info('connecting to DB')
+        connect()
+        logger.info('setting parameters')
+        set_params()
+        logger.info('checking height')
+        check_node_height()
+        logger.info('calculating payouts')
+        rawpayouts, timestamp = calculate()
+        logger.info('formatting payouts')
+        formatted_payouts, delegate_share = format_payments(
+            payouts=rawpayouts,
+            timestamp=timestamp
+        )
+        if config.PAYOUTCALCULATOR_TEST:
+            logger.info('FORMATTED PAYMENTS')
+            for i in formatted_payouts:
+                logger.info('{} ---- {}'.format(i, formatted_payouts[i]/info.ARK))
+
+        else:
+            logger.info('transmitting payouts')
+            transmit_payments(
+                payouts=formatted_payouts
+                )
+            logger.info('sending delegate share')
+            send_delegate_share(
+                amount=delegate_share
+            )
     except Exception as e:
         pass
         logger.fatal('caught exception in plugandplay: {}'.format(e))
